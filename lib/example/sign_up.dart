d0756095a70403c592b43bf6337c365b8a66b2ed
iamlinkanyadav <stefenrobert70@gmail.com>
2023-10-25 19:14:49 +0530

    Sign Up Api Practice

--- lib/example/sign_up.dart
+++ lib/example/sign_up.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -8,6 +11,28 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  void login(String email, password) async {
+    try {
+      Response response = await post(
+        Uri.parse('https://reqres.in/api/register'),
+        body: {
+          'email': email,
+          'password': password,
+        },
+      );
+      if (response.statusCode == 200) {
+        print('Account Created Succesfully !');
+      } else {
+        print('Failed !');
+      }
+      print('object');
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,10 +40,50 @@ class _SignUpScreenState extends State<SignUpScreen> {
         title: const Text('Sign Up Api'),
         centerTitle: true,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                hintText: 'Email',
+                prefixIcon: Icon(Icons.email_outlined),
+                enabledBorder: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: passController,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+                prefixIcon: Icon(Icons.lock_outlined),
+                enabledBorder: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 40),
+            GestureDetector(
+              onTap: () {
+                login(emailController.text.toString(),
+                    passController.text.toString());
+              },
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text('Sign Up'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
