8d9cde954ec0ef24ed43241350c8dca83e0d3da3
iamlinkanyadav <stefenrobert70@gmail.com>
2024-03-16 16:13:13 +0530

    Ui Desgine #Day1

---
+++ lib/example/practice_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PracticeScreen());
+}
+
+class PracticeScreen extends StatelessWidget {
+  const PracticeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Good Morning'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+    );
+  }
+}
